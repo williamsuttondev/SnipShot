da47e365dbb73595c4719b55e730ae038a1e5b1b
William Sutton <williamace@live.co.uk>
2024-08-02 22:29:13 +0100

    Added the imports necessary to build the app

---
+++ SnippingTool.py
@@ -0,0 +1,5 @@
+import sys
+from PyQt5.QtCore import Qt, QRect
+from PyQt5.QtGui import QPixmap, QPainter, QColor, QClipboard
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QWidget, QDialog, QPushButton, QVBoxLayout
+
