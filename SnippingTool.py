c75bffc06b435e87f66f32553d3d9f54c1c2acdd
William Sutton <williamace@live.co.uk>
2024-08-02 22:32:23 +0100

    Added initial Capture Screenshot window

--- SnippingTool.py
+++ SnippingTool.py
@@ -3,3 +3,18 @@ from PyQt5.QtCore import Qt, QRect
 from PyQt5.QtGui import QPixmap, QPainter, QColor, QClipboard
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QWidget, QDialog, QPushButton, QVBoxLayout
 
+
+class SnippingTool(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Snipping Tool')
+        self.setGeometry(100, 100, 300, 100)
+        self.start_button = QPushButton('Capture Screenshot', self)
+        self.start_button.setGeometry(50, 20, 200, 60)
+        # self.start_button.clicked.connect(self.start_snipping)
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    snipping_tool = SnippingTool()
+    sys.exit(app.exec_())
