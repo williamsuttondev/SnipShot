d6816bdcd4a1cd3cbc40c5d02470f2b32603b49f
William Sutton <williamace@live.co.uk>
2024-08-02 22:35:48 +0100

    Initial Screenshot button hides the window, now need to make snipping window to capture screenshots

--- SnippingTool.py
+++ SnippingTool.py
@@ -11,8 +11,14 @@ class SnippingTool(QMainWindow):
         self.setGeometry(100, 100, 300, 100)
         self.start_button = QPushButton('Capture Screenshot', self)
         self.start_button.setGeometry(50, 20, 200, 60)
-        # self.start_button.clicked.connect(self.start_snipping)
+        self.start_button.clicked.connect(self.start_snipping)
         self.show()
+
+    def start_snipping(self):
+        self.hide()
+        #self.snipping_window = SnippingWindow(self)
+        #self.snipping_window.showFullScreen()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
